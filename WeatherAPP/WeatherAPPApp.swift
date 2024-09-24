5478487751ab9a2141735319e525406efdb58940
MoniqueFer <138914572+MoniqueFer@users.noreply.github.com>
2024-09-24 11:42:22 -0300

    Initial Commit

---
+++ WeatherAPP/WeatherAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPPApp.swift
+//  WeatherAPP
+//
+//  Created by Monique Ferrarini on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherAPPTests/WeatherAPPTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPPTests.swift
+//  WeatherAPPTests
+//
+//  Created by Monique Ferrarini on 24/09/24.
+//
+
+import Testing
+@testable import WeatherAPP
+
+struct WeatherAPPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
